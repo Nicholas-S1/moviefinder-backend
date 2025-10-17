5a87cb0a764ea08352b2a12113b52e2bb4fb3d64
Nick Setters <njs154@uakron.edu>
2025-10-17 19:23:32 -0400

    Initial Movie Finder backend

---
+++ db.js
@@ -0,0 +1,16 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+console.log('Environment PORT:', process.env.PORT);
+console.log('Database:', process.env.PGDATABASE);
