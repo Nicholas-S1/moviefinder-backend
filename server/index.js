48fff7f4e556f7c33521d65afeb92a289e2da04c
Nick Setters <njs154@uakron.edu>
2025-10-17 21:12:01 -0400

    Completely remove bcrypt and switch to bcryptjs for Render

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import pkg from 'pg';
 const { Pool } = pkg;
 
