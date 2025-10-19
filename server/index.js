40900f0a5fa527004e2cbce1695727f23f3221f7
Nick Setters <njs154@uakron.edu>
2025-10-18 21:54:08 -0400

    Fix pool export/import for Render

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import bcrypt from 'bcryptjs';
-import { pool } from './db.js'; // ✅ Use the shared connection here
+import  pool from './db.js'; // ✅ Use the shared connection here
 
 // =======================
 //  Environment Setup
